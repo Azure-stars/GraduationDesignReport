a699ae0c397e36e2a46d8adc9c9d0e1a1840c183
Youjie Zheng <zyj578335934@gmail.com>
2025-03-06 17:49:09 +0800

    [feat] Add analysis for syscall

---
+++ parse_syscall/main.py
@@ -0,0 +1,27 @@
+# Description: Extract syscall list from strace log file
+import argparse
+argparser = argparse.ArgumentParser()
+argparser.add_argument('-s', '--strace_log', type=str, help='strace log file')
+args = argparser.parse_args()
+
+STRACE_LOG = args.strace_log
+SYSCALL_LIST = []
+
+with open(STRACE_LOG + '.log', 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        if '(' not in line:
+            continue
+        # If using strace -f to trace, the first field of each line is the pid, so we need to remove it
+        if line[0].isdigit():
+            line = line.split(' ', 1)[1]
+        syscall = line.split('(')[0]
+        # Remove useless information
+        if syscall.find('<') != -1 or syscall.find('+') != -1:
+            continue
+        if syscall not in SYSCALL_LIST:
+            SYSCALL_LIST.append(syscall)
+
+with open(STRACE_LOG + '_syscall_list.txt', 'w') as f:
+    for syscall in SYSCALL_LIST:
+        f.write(syscall + '\n')
